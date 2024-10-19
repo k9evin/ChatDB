4a8e0023a3f663b209bb218a339f4118d46f73ea
Mingkai Pang <pangmingkai@gmail.com>
2024-10-18 17:23:50 -0700

    Add CORS middleware to allow requests from different port

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 from app.config import settings
 import logging
 
 app = FastAPI()
+
+# Allow Cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 if __name__ == "__main__":
